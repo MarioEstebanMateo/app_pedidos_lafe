ad448383c618633c70dc42d01979df53b66aad87
Mario Esteban Mateo <memateo@gmail.com>
2023-09-01 13:59:11 -0300

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p className="footerP">
+        &copy; 2023 Todos los derechos reservados :: Desarrollado por Mario
+      </p>
+    </div>
+  )
+}
+
+export default Footer
